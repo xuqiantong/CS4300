4ddb9e8f78223924c0351c10e890f70a9a952401
jeffliu6 <jl2964@cornell.edu>
2019-04-10 15:11:07 -0400

    db_preprocess setup

--- scripts/db_preprocess.py
+++ scripts/db_preprocess.py
@@ -2,6 +2,26 @@ import json
 
 
 if __name__ == "__main__":
-    with open('../data/allbud_output.json') as f:
-        data = json.load(f)
-        print(len(data))
+    allbud_data = {}
+    otreeba_strains_data = []
+    otreeba_conditions_data = []
+    otreeba_studies_data = []
+    leafly_data = {}
+
+
+    with open('../data/allbud_output.json', encoding="utf8") as f:
+        allbud_data = json.load(f)
+
+    with open('../data/strains.json', encoding="utf8") as f:
+        otreeba_strains_data = json.load(f)['data']
+
+    with open('../data/conditions.json', encoding="utf8") as f:
+        otreeba_conditions_data = json.load(f)['data']
+
+    with open('../data/studies.json', encoding="utf8") as f:
+        otreeba_studies_data = json.load(f)['data']
+
+    print(len(allbud_data))
+    print(len(otreeba_strains_data))
+    print(len(otreeba_conditions_data))
+    print(len(otreeba_studies_data))
